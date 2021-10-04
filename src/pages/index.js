b8c76476b06c7ad3c4013d5129c5d1186e81a377
Deiv <77246331+dubisdev@users.noreply.github.com>
2021-10-04 11:04:47 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ function HomepageHeader() {
 	return (
 		<header className={clsx("hero hero--primary", styles.heroBanner)}>
 			<div className="container">
-				<h1 className="hero__title">{siteConfig.title}</h1>
+				<h1 className="hero__title">{siteConfig.title} by @dubisdev</h1>
 				<p className="hero__subtitle">{siteConfig.tagline}</p>
 			</div>
 		</header>
